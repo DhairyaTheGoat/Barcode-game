e8744bdc04bd1f12d5128922c923c6df6bfd5632
DhairyaTheGoat <131680259+DhairyaTheGoat@users.noreply.github.com>
2023-09-04 20:27:24 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,39 @@
-var jim
-var player
+var jim,jimImg
 var ground
-var obstacles
+var obstacles,obstacleImg
+var platform
 
 function preload(){
-
+obstaclesImg = loadImage("Obstacles.png")
+jimImg = loadImage("Stickman.png")
 }
 
 function setup(){
 createCanvas(600,500)
 jim = createSprite(100,450,80,80)
+jim.addImage("Useless",jimImg)
 ground = createSprite(300,490,700,10)
+platform = createSprite(150,250,70,10)
 }
 
 function draw(){
 background("gray")
 spawnObstacles()
+
+if(keyDown==="UP_ARROW"){
+platform.y = platform.y + 3  
+}
+
+if(keyDown==="DOWN_ARROW"){
+platform.y = platform.y - 3
+}
 drawSprites()
 }
 
 function spawnObstacles(){
 if(frameCount%20===0){
 obstacles = createSprite(600,random(0,500),30,50)
+obstacles.addImage("obstacle",obstaclesImg)
 obstacles.velocityX = -2;
 }
 }
